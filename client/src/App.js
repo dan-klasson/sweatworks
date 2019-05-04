ef608e37462b257245b07a077ca6bc240de95870
Dan Klasson <dan.g.klasson@gmail.com>
2019-05-04 16:39:48 +0800

    added bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,43 @@
 import React, { Suspense } from 'react'
+import {
+  Container,
+  Row,
+  Col,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink} from 'reactstrap';
 import './App.css'
 import Author from './Author'
 
 
 function App() {
   return (
-    <Suspense fallback="Loading...">
-      <Author />
-    </Suspense>
+    <div>
+      <Navbar color="dark" dark expand="md">
+        <NavbarBrand href="/">sweatworks</NavbarBrand>
+        <NavbarToggler />
+        <Collapse navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/"></NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+
+      <Suspense fallback="Loading...">
+        <Container>
+          <Row>
+            <Col xs="3"><Author /></Col>
+            <Col xs="auto"></Col>
+          </Row>
+        </Container>
+      </Suspense>
+    </div>
   )
 }
 
